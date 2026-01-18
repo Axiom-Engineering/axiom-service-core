76ff0c993878faa0629db3bf8d5954a7cf847bbd
sameeraherath <sameerastar6@gmail.com>
2026-01-18 10:44:12 +0530

    Initial commit

---
+++ src/main/java/com/axiom/core/AxiomServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.axiom.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiomServiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxiomServiceCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axiom/core/AxiomServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiom.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxiomServiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
